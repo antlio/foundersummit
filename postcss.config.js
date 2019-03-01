1627fc356707be5498d7ff042653de3d3258ea5e
Anthony Lionnet <lionnet.ant@gmail.com>
2019-03-01 16:25:57 +0100

    Import postcss plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,16 @@
 const autoprefixer = require('autoprefixer')
 const postcssCustomMedia = require('postcss-custom-media')
 const postcssNested = require('postcss-nested')
+const postcssFor = require('postcss-for')
+const postcssRandom = require('postcss-random')
+const postcssCalc = require('postcss-calc')
 
 module.exports = {
   plugins: [
     autoprefixer({}),
+    postcssFor({}),
+    postcssRandom({}),
+    postcssCalc({}),
     postcssCustomMedia({
       importFrom: 'assets/stylesheets/base/media.css'
     }),
