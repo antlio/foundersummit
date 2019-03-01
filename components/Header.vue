6e7cd4e8c7053ff3ec27d9d4a4790e1e5bad57d0
Anthony Lionnet <lionnet.ant@gmail.com>
2019-03-01 09:26:04 +0100

    Add blank target to adress link

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,7 @@
     <div class="__logo">
       <img src="../assets/images/le-wagon-smart-up-logo.svg" alt="Le wagon x Smart up logo">
     </div>
-    <a href="http://maps.google.com/maps?q=20+rue+des+capucins,+69001+lyon" class="__adress">20 rue des capucins, 69001 Lyon</a>
+    <a href="http://maps.google.com/maps?q=20+rue+des+capucins,+69001+lyon" target="_blank" class="__adress">20 rue des capucins, 69001 Lyon</a>
     <div class="__navigation">
       <a class="__navigation--underline" href="#">Intervenants</a>
       <a class="__navigation--underline" href="#">Contact</a>
