e53380f76a3979e99b155f6205a57e38667b5c5c
Anthony Lionnet <lionnet.ant@gmail.com>
2019-03-01 09:23:11 +0100

    Import header component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,27 @@
 <template>
   <div>
+    <app-header></app-header>
     <nuxt />
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+  components: {
+    'app-header': Header
+  }
+}
+</script>
+
+
 <style lang="postcss">
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+  font-family: 'Graphik Regular', 'Helvetica', sans-serif;
+  font-size: var(--font-base);
+  letter-spacing: 0.5px;
+  padding: 0 var(--spacing-base);
 }
 
 *,
@@ -22,34 +29,5 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
