a2f10573cd6571571b56a8eb7465d98bc012a266
Anthony Lionnet <lionnet.ant@gmail.com>
2019-03-02 16:06:42 +0100

    Edit single quote to double

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
-const autoprefixer = require('autoprefixer')
-const postcssCustomMedia = require('postcss-custom-media')
-const postcssNested = require('postcss-nested')
-const postcssFor = require('postcss-for')
-const postcssRandom = require('postcss-random')
-const postcssCalc = require('postcss-calc')
+const autoprefixer = require("autoprefixer");
+const postcssCustomMedia = require("postcss-custom-media");
+const postcssNested = require("postcss-nested");
+const postcssFor = require("postcss-for");
+const postcssRandom = require("postcss-random");
+const postcssCalc = require("postcss-calc");
 
 module.exports = {
   plugins: [
@@ -12,10 +12,10 @@ module.exports = {
     postcssRandom({}),
     postcssCalc({}),
     postcssCustomMedia({
-      importFrom: 'assets/stylesheets/base/media.css'
+      importFrom: "assets/stylesheets/base/media.css"
     }),
     postcssNested({
-      importFrom: 'assets/stylesheets/application.css'
+      importFrom: "assets/stylesheets/application.css"
     })
   ]
-}
+};
