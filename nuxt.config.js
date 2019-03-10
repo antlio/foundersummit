e0efa7094f9cd2f033af7b630371ce9c58171573
Anthony Lionnet <lionnet.ant@gmail.com>
2019-03-10 19:18:47 +0100

    Add push asset font

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,10 +58,14 @@ module.exports = {
   */
   render: {
     http2: {
-      push: true,
       pushAssets: (req, res, publicPath, preloadFiles) =>
         preloadFiles
-          .filter(f => f.asType === "script" || f.asType === "+xml")
+          .filter(
+            f =>
+              f.asType === "script" &&
+              f.file === "runtime.js" &&
+              f.asType === "font"
+          )
           .map(f => `<${publicPath}${f.file}>; rel=preload; as=${f.asType}`)
     }
   },
