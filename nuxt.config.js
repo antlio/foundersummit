081b45ba6769dc32cb5ec8750a2664ee62c5abb1
Anthony Lionnet <lionnet.ant@gmail.com>
2019-03-11 16:58:42 +0100

    Import cookie plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,10 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [{ src: "~plugins/smooth-scroll", ssr: false }],
+  plugins: [
+    { src: "~/plugins/cookie-consent", ssr: false },
+    { src: "~plugins/smooth-scroll", ssr: false }
+  ],
 
   /*
   ** Nuxt.js modules
