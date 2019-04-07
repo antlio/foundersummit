c167a79b460253a9b34e4a8f5096a91726e07ea8
Anthony Lionnet <lionnet.ant@gmail.com>
2019-04-07 12:45:51 +0200

    Edit button content

--- components/Modal.vue
+++ components/Modal.vue
@@ -38,7 +38,7 @@
               <svg v-if="loading" class="spinner" viewBox="0 0 50 50">
                 <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
               </svg>
-              <span v-else id="post">S'inscrire</span>
+              <span v-else id="post">Être tenu informé</span>
             </button>
           </form>
         </div>
